8cd15ce189a3c75ce0f602cab035488c06e378ab
Benjamin Bertrand <beenje@gmail.com>
2023-07-23 16:21:37 +0200

    Fix jupyterlab_gitlab server extension path
    labextension is no longer installed under the python package.
    It's copied under share/jupyter/labextensions/jupyterlab-gitlab.

--- jupyterlab_gitlab/__init__.py
+++ jupyterlab_gitlab/__init__.py
@@ -1,17 +1,9 @@
-import json
-from pathlib import Path
-
 from ._version import __version__  # noqa
 from .gitlab import setup_handlers
 
-HERE = Path(__file__).parent.resolve()
-
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
-
 
 def _jupyter_labextension_paths():
-    return [{"src": "labextension", "dest": data["name"]}]
+    return [{"src": "labextension", "dest": "jupyterlab-gitlab"}]
 
 
 def _jupyter_server_extension_points():
@@ -27,6 +19,8 @@ def _load_jupyter_server_extension(server_app):
         JupyterLab application instance
     """
     setup_handlers(server_app.web_app)
+    name = "jupyterlab_gitlab"
+    server_app.log.info(f"Registered {name} server extension")
 
 
 # For backward compatibility with notebook server - useful for Binder/JupyterHub
